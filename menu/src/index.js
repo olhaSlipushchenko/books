c4b8a432716f8b5c936a11652c6c1a1138f90547
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2023-06-19 20:55:30 +0300

    main

---
+++ menu/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import  './index.css';
+import { books } from './books'; 
+import Book from './Book';
+import HeadLine from './HeadLine';
+
+const BookList = () => {
+  return (
+    <>
+      <HeadLine /> 
+      <section className="booklist">
+        <EventExamples />
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number= {index}/>;
+        })}
+      </section>
+    </>
+  );
+};
+const EventExamples = () => {
+  return <section> 
+    <form>
+      <h2> typical form</h2>
+      <input 
+       type="text"
+       name='example'
+       onChange={(e) => console.log(e.target.value)}
+       style= {{margin: '1rem 0'}} />
+       <button type='submit'> submit form</button>
+      </form>
+      <button onClick= {() => console.log('click me')} >click me</button>
+  </section>
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList />)
